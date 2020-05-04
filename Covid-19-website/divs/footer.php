29eaeeb9f24eb1025d8c68e37f9035feff50b5ef
gkrmadrid <madridkarlo7@gmail.com>
2020-05-04 22:35:49 +0800

    Configured final folder name for DAL

--- Covid-19-website/divs/footer.php
+++
@@ -1,7 +0,0 @@
-<div class="footer-container">
-	<div class="footer-content">
-		<div class="footer-left-box">
-			<a href="">Contact Us</a>
-		</div>
-	</div>
-</div>
--- chatbot_website/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChatbotWebsiteConfig(AppConfig):
-    name = 'chatbot_website'
